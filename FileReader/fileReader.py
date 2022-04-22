b34f3f1ea6eeab903ccef830cb7037d852c0092b
ccnuktd <nana414021069@163.com>
2022-04-22 14:45:38 +0800

    新增FileReader小工具

---
+++ FileReader/fileReader.py
@@ -0,0 +1,40 @@
+import csv
+import sys
+
+
+def read_file_line(file_path: str, encoding='utf-8'):
+    """
+    逐行读取文件数据，默认以utf-8编码读取
+    用此方法读出的line为str类型
+    :param encoding:
+    :param file_path:
+    :return:
+    """
+
+    with open(file_path, 'r', encoding=encoding) as f:
+        lines = f.readlines()
+        for line in lines:
+            yield line
+
+def read_csv_line(file_path: str, encoding='utf-8'):
+    """
+    逐行读取csv文件数据，默认以utf-8编码读取
+    用此方法读出的line为list
+    :param file_path:
+    :param encoding:
+    :return:
+    """
+
+    with open(file_path, 'r', encoding=encoding) as f:
+        reader = csv.reader(f)
+        # 跳过首行
+        next(reader)
+        for line in reader:
+            yield line
+
+if __name__ == '__main__':
+    # for line in read_file_line('./data.csv'):
+    #     print(line)
+
+    # for line in read_csv_line('./data.csv'):
+    #     print(line)
